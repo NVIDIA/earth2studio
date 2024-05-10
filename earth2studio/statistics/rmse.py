683d19de888940c324f7d23baa02eeaf416e43f7
Dallas Foster <dallasf@nvidia.com>
2024-05-10 12:17:27 -0700

    Add spread/skill ratio statistic (#44)
    * add spread skill and improve test coverage
    
    * remove print
    
    * update changelog
    
    * update docs

--- earth2studio/statistics/rmse.py
+++ earth2studio/statistics/rmse.py
@@ -18,7 +18,7 @@ import torch
 
 from earth2studio.utils.type import CoordSystem
 
-from .moments import mean
+from .moments import mean, variance
 
 
 class rmse:
@@ -40,7 +40,7 @@ class rmse:
     batch_update: bool = False
         Whether to applying batch updates to the rmse with each invocation of __call__.
         This is particularly useful when data is recieved in a stream of batches. Each
-        invocation of __call__ will return the running rmse. It particular, it will apply
+        invocation of __call__ will return the running rmse. In particular, it will apply
         the square root operation after calculating the running mean squared error.
     """
 
@@ -96,3 +96,118 @@ class rmse:
         """
         mse, output_coords = self.mean((x - y) ** 2, x_coords)
         return torch.sqrt(mse), output_coords
+
+
+class spread_skill_ratio:
+    """
+     Metric for calculating the spread/skill ratio of an ensemble forecast.
+
+     Specifically, the spread is defined as the standard deviation of the ensemble
+     forecast. The skill is defined as the rmse of the ensemble mean prediction. The
+     ratio of these two quantities is defined as the spread/skill ratio.
+
+
+     Parameters
+     ----------
+     ensemble_dimension: str
+         The dimension over which the spread and skill are calculated over.
+         This should usually be "ensemble".
+     reduction_dimensions: list[str]
+         Dimensions to reduce (mean) the spread/skill ratio over. This is commonly done
+         over time but can also be the globe or some region.
+         Example ['time', 'lat', 'lon']
+     ensemble_weights: torch.Tensor = None
+         A one-dimensional tensor containing weights to assign to the
+         ensemble_dimension.
+     reduction_weights: torch.Tensor = None
+         A tensor containing weights to assign to the reduction dimensions.
+         Note that these weights must have the same number of dimensions
+         as passed in reduction_dimensions.
+         Example: if reduction_dimensions = ['lat', 'lon'] then
+         assert weights.ndim == 2.
+         If reduction_dimensions is None, then this argument is ignored.
+    ensemble_batch_update: bool = False
+         Whether to applying batch updates to the ensemble mean and variance components of
+         the spread and skill with each invocation of __call__.
+         This is particularly useful when ensemble data is recieved in a stream of batches.
+         Each invocation of __call__ will return the running spread/skill ratio.
+     batch_update: bool = False
+         Whether to applying batch updates to the reduction rmse and averaging components
+         of the spread/skill with each invocation of __call__.
+         This is particularly useful when time data is recieved in a stream of batches.
+    """
+
+    def __init__(
+        self,
+        ensemble_dimension: str,
+        reduction_dimensions: list[str],
+        ensemble_weights: torch.Tensor | None = None,
+        reduction_weights: torch.Tensor = None,
+        ensemble_batch_update: bool = False,
+        reduction_batch_update: bool = False,
+    ):
+        self.ensemble_dimension = [ensemble_dimension]
+        self.reduction_dimensions = reduction_dimensions
+        self.ensemble_mean = mean(
+            reduction_dimensions=self.ensemble_dimension,
+            weights=ensemble_weights,
+            batch_update=ensemble_batch_update,
+        )
+        self.ensemble_var = variance(
+            reduction_dimensions=self.ensemble_dimension,
+            weights=ensemble_weights,
+            batch_update=ensemble_batch_update,
+        )
+        self.reduced_rmse = rmse(
+            reduction_dimensions=reduction_dimensions,
+            weights=reduction_weights,
+            batch_update=reduction_batch_update,
+        )
+        self.reduced_mean = mean(
+            reduction_dimensions=reduction_dimensions,
+            weights=reduction_weights,
+            batch_update=reduction_batch_update,
+        )
+
+    def __str__(self) -> str:
+        return "_".join(
+            self.ensemble_dimension + self.reduction_dimensions + ["spread_skill"]
+        )
+
+    def __call__(
+        self,
+        x: torch.Tensor,
+        x_coords: CoordSystem,
+        y: torch.Tensor,
+        y_coords: CoordSystem,
+    ) -> tuple[torch.Tensor, CoordSystem]:
+        """
+        Apply metric to data `x` and `y`, checking that their coordinates
+        are broadcastable. While reducing over `reduction_dims`.
+
+        If batch_update was passed True upon metric initialization then this method
+        returns the running sample RMSE over all seen batches.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor #1 intended to apply metric to.
+        x_coords : CoordSystem
+            Ordered dict representing coordinate system that describes the `x` tensor.
+            'reduction_dims' must be in coords.
+        y : torch.Tensor
+            Input tensor #2 intended to apply statistic to.
+        y_coords : CoordSystem
+            Ordered dict representing coordinate system that describes the `y` tensor.
+            'reduction_dims' must be in coords.
+
+        Returns
+        -------
+        tuple[torch.Tensor, CoordSystem]
+            Returns root mean squared error tensor with appropriate reduced coordinates.
+        """
+
+        em, output_coords = self.ensemble_mean(x, x_coords)
+        skill, output_coords = self.reduced_rmse(em, output_coords, y, y_coords)
+        spread, output_coords = self.reduced_mean(*self.ensemble_var(x, x_coords))
+        return skill / torch.sqrt(spread), output_coords
