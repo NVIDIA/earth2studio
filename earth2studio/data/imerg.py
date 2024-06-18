cf59f9691960e040bfd53926afd3ebec16b16ba2
Nicholas Geneva <5533524+NickGeneva@users.noreply.github.com>
2024-06-18 10:11:30 -0700

    Updating IMERG total precip data to meters (#76)

--- earth2studio/data/imerg.py
+++ earth2studio/data/imerg.py
@@ -40,10 +40,10 @@ LOCAL_CACHE = os.path.join(os.path.expanduser("~"), ".cache", "earth2studio")
 
 
 class IMERG:
-    """The Integrated Multi-satellitE Retrievals (IMERG) for GPM. MERG is a NASA product
-    that estimates the global surface precipitation rates at a high resolution of 0.1
-    degree every half-hour beginning in 2000. Provides total, probability and index
-    precipitation fields for the past half hour.
+    """The Integrated Multi-satellitE Retrievals (IMERG) for GPM. IMERG is a NASA
+    product that estimates the global surface precipitation rates at a high resolution
+    of 0.1 degree every half-hour beginning in 2000. Provides total (m/hr), probability
+    and index precipitation fields for the past half hour.
 
     Note
     ----
