97f80d43604832d074ad76351b419f1bda181fbe
Nicholas Geneva <5533524+NickGeneva@users.noreply.github.com>
2024-08-12 14:24:04 -0700

    Adding GEFS forecast data source (#112)
    * Adding GEFS data source
    * Adding tests for GEFS
    * Feedback
    * Fixes

---
+++ earth2studio/lexicon/gefs.py
@@ -0,0 +1,239 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from collections.abc import Callable
+
+import numpy as np
+
+from .base import LexiconType
+
+
+class GEFSLexicon(metaclass=LexiconType):
+    """Global Ensemble Forecast System Lexicon, right now only support isobarric
+    GEFS specified <Parameter ID>::<Level/ Layer>
+
+    Note
+    ----
+    Additional resources:
+    - https://www.nco.ncep.noaa.gov/pmb/products/gens/gec00.t00z.pgrb2a.0p50.f003.shtml
+    - https://www.nco.ncep.noaa.gov/pmb/products/gens/gec00.t00z.pgrb2b.0p50.f003.shtml
+    """
+
+    VOCAB = {
+        "u10m": "pgrb2a::UGRD::10 m above ground",
+        "v10m": "pgrb2a::VGRD::10 m above ground",
+        "u100m": "pgrb2b::UGRD::100 m above ground",
+        "v100m": "pgrb2b::VGRD::100 m above ground",
+        "t2m": "pgrb2a::TMP::2 m above ground",
+        "t100m": "pgrb2b::TMP::100 m above ground",
+        "sp": "pgrb2a::PRES::surface",
+        "msl": "pgrb2b::PRES::mean sea level",
+        "tcwv": "pgrb2a::PWAT::entire atmosphere (considered as a single layer)",
+        "u1": "pgrb2b::UGRD::1 mb",
+        "u2": "pgrb2b::UGRD::2 mb",
+        "u3": "pgrb2b::UGRD::3 mb",
+        "u5": "pgrb2b::UGRD::5 mb",
+        "u7": "pgrb2b::UGRD::7 mb",
+        "u10": "pgrb2a::UGRD::10 mb",
+        "u20": "pgrb2b::UGRD::20 mb",
+        "u30": "pgrb2b::UGRD::30 mb",
+        "u50": "pgrb2a::UGRD::50 mb",
+        "u70": "pgrb2b::UGRD::70 mb",
+        "u100": "pgrb2a::UGRD::100 mb",
+        "u150": "pgrb2b::UGRD::150 mb",
+        "u200": "pgrb2a::UGRD::200 mb",
+        "u250": "pgrb2a::UGRD::250 mb",
+        "u300": "pgrb2a::UGRD::300 mb",
+        "u350": "pgrb2b::UGRD::350 mb",
+        "u400": "pgrb2a::UGRD::400 mb",
+        "u450": "pgrb2b::UGRD::450 mb",
+        "u500": "pgrb2a::UGRD::500 mb",
+        "u550": "pgrb2b::UGRD::550 mb",
+        "u600": "pgrb2b::UGRD::600 mb",
+        "u650": "pgrb2b::UGRD::650 mb",
+        "u700": "pgrb2a::UGRD::700 mb",
+        "u750": "pgrb2b::UGRD::750 mb",
+        "u800": "pgrb2b::UGRD::800 mb",
+        "u850": "pgrb2a::UGRD::850 mb",
+        "u900": "pgrb2b::UGRD::900 mb",
+        "u925": "pgrb2a::UGRD::925 mb",
+        "u950": "pgrb2b::UGRD::950 mb",
+        "u975": "pgrb2b::UGRD::975 mb",
+        "u1000": "pgrb2a::UGRD::1000 mb",
+        "v1": "pgrb2b::VGRD::1 mb",
+        "v2": "pgrb2b::VGRD::2 mb",
+        "v3": "pgrb2b::VGRD::3 mb",
+        "v5": "pgrb2b::VGRD::5 mb",
+        "v7": "pgrb2b::VGRD::7 mb",
+        "v10": "pgrb2a::VGRD::10 mb",
+        "v20": "pgrb2b::VGRD::20 mb",
+        "v30": "pgrb2b::VGRD::30 mb",
+        "v50": "pgrb2a::VGRD::50 mb",
+        "v70": "pgrb2b::VGRD::70 mb",
+        "v100": "pgrb2a::VGRD::100 mb",
+        "v150": "pgrb2b::VGRD::150 mb",
+        "v200": "pgrb2a::VGRD::200 mb",
+        "v250": "pgrb2a::VGRD::250 mb",
+        "v300": "pgrb2a::VGRD::300 mb",
+        "v350": "pgrb2b::VGRD::350 mb",
+        "v400": "pgrb2a::VGRD::400 mb",
+        "v450": "pgrb2b::VGRD::450 mb",
+        "v500": "pgrb2a::VGRD::500 mb",
+        "v550": "pgrb2b::VGRD::550 mb",
+        "v600": "pgrb2b::VGRD::600 mb",
+        "v650": "pgrb2b::VGRD::650 mb",
+        "v700": "pgrb2a::VGRD::700 mb",
+        "v750": "pgrb2b::VGRD::750 mb",
+        "v800": "pgrb2b::VGRD::800 mb",
+        "v850": "pgrb2a::VGRD::850 mb",
+        "v900": "pgrb2b::VGRD::900 mb",
+        "v925": "pgrb2a::VGRD::925 mb",
+        "v950": "pgrb2b::VGRD::950 mb",
+        "v975": "pgrb2b::VGRD::975 mb",
+        "v1000": "pgrb2a::VGRD::1000 mb",
+        "z1": "pgrb2b::HGT::1 mb",
+        "z2": "pgrb2b::HGT::2 mb",
+        "z3": "pgrb2b::HGT::3 mb",
+        "z5": "pgrb2b::HGT::5 mb",
+        "z7": "pgrb2b::HGT::7 mb",
+        "z10": "pgrb2a::HGT::10 mb",
+        "z20": "pgrb2b::HGT::20 mb",
+        "z30": "pgrb2b::HGT::30 mb",
+        "z50": "pgrb2a::HGT::50 mb",
+        "z70": "pgrb2b::HGT::70 mb",
+        "z100": "pgrb2a::HGT::100 mb",
+        "z150": "pgrb2b::HGT::150 mb",
+        "z200": "pgrb2a::HGT::200 mb",
+        "z250": "pgrb2a::HGT::250 mb",
+        "z300": "pgrb2a::HGT::300 mb",
+        "z350": "pgrb2b::HGT::350 mb",
+        "z400": "pgrb2b::HGT::400 mb",
+        "z450": "pgrb2b::HGT::450 mb",
+        "z500": "pgrb2a::HGT::500 mb",
+        "z550": "pgrb2b::HGT::550 mb",
+        "z600": "pgrb2b::HGT::600 mb",
+        "z650": "pgrb2b::HGT::650 mb",
+        "z700": "pgrb2a::HGT::700 mb",
+        "z750": "pgrb2b::HGT::750 mb",
+        "z800": "pgrb2b::HGT::800 mb",
+        "z850": "pgrb2a::HGT::850 mb",
+        "z900": "pgrb2b::HGT::900 mb",
+        "z925": "pgrb2a::HGT::925 mb",
+        "z950": "pgrb2b::HGT::950 mb",
+        "z975": "pgrb2b::HGT::975 mb",
+        "z1000": "pgrb2a::HGT::1000 mb",
+        "t1": "pgrb2b::TMP::1 mb",
+        "t2": "pgrb2b::TMP::2 mb",
+        "t3": "pgrb2b::TMP::3 mb",
+        "t5": "pgrb2b::TMP::5 mb",
+        "t7": "pgrb2b::TMP::7 mb",
+        "t10": "pgrb2a::TMP::10 mb",
+        "t20": "pgrb2b::TMP::20 mb",
+        "t30": "pgrb2b::TMP::30 mb",
+        "t50": "pgrb2a::TMP::50 mb",
+        "t70": "pgrb2b::TMP::70 mb",
+        "t100": "pgrb2a::TMP::100 mb",
+        "t150": "pgrb2b::TMP::150 mb",
+        "t200": "pgrb2a::TMP::200 mb",
+        "t250": "pgrb2a::TMP::250 mb",
+        "t300": "pgrb2b::TMP::300 mb",
+        "t350": "pgrb2b::TMP::350 mb",
+        "t400": "pgrb2b::TMP::400 mb",
+        "t450": "pgrb2b::TMP::450 mb",
+        "t500": "pgrb2a::TMP::500 mb",
+        "t550": "pgrb2b::TMP::550 mb",
+        "t600": "pgrb2b::TMP::600 mb",
+        "t650": "pgrb2b::TMP::650 mb",
+        "t700": "pgrb2a::TMP::700 mb",
+        "t750": "pgrb2b::TMP::750 mb",
+        "t800": "pgrb2b::TMP::800 mb",
+        "t850": "pgrb2a::TMP::850 mb",
+        "t900": "pgrb2b::TMP::900 mb",
+        "t925": "pgrb2a::TMP::925 mb",
+        "t950": "pgrb2b::TMP::950 mb",
+        "t975": "pgrb2b::TMP::975 mb",
+        "t1000": "pgrb2a::TMP::1000 mb",
+        "r10": "pgrb2a::RH::10 mb",
+        "r20": "pgrb2b::RH::20 mb",
+        "r30": "pgrb2b::RH::30 mb",
+        "r50": "pgrb2a::RH::50 mb",
+        "r70": "pgrb2b::RH::70 mb",
+        "r100": "pgrb2a::RH::100 mb",
+        "r150": "pgrb2b::RH::150 mb",
+        "r200": "pgrb2a::RH::200 mb",
+        "r250": "pgrb2a::RH::250 mb",
+        "r300": "pgrb2b::RH::300 mb",
+        "r350": "pgrb2b::RH::350 mb",
+        "r400": "pgrb2b::RH::400 mb",
+        "r450": "pgrb2b::RH::450 mb",
+        "r500": "pgrb2a::RH::500 mb",
+        "r550": "pgrb2b::RH::550 mb",
+        "r600": "pgrb2b::RH::600 mb",
+        "r650": "pgrb2b::RH::650 mb",
+        "r700": "pgrb2a::RH::700 mb",
+        "r750": "pgrb2b::RH::750 mb",
+        "r800": "pgrb2b::RH::800 mb",
+        "r850": "pgrb2a::RH::850 mb",
+        "r900": "pgrb2b::RH::900 mb",
+        "r925": "pgrb2a::RH::925 mb",
+        "r950": "pgrb2b::RH::950 mb",
+        "r975": "pgrb2b::RH::975 mb",
+        "r1000": "pgrb2a::RH::1000 mb",
+        "q10": "pgrb2b::SPFH::10 mb",
+        "q20": "pgrb2b::SPFH::20 mb",
+        "q30": "pgrb2b::SPFH::30 mb",
+        "q50": "pgrb2b::SPFH::50 mb",
+        "q70": "pgrb2b::SPFH::70 mb",
+        "q100": "pgrb2b::SPFH::100 mb",
+        "q150": "pgrb2b::SPFH::150 mb",
+        "q200": "pgrb2b::SPFH::200 mb",
+        "q250": "pgrb2b::SPFH::250 mb",
+        "q300": "pgrb2b::SPFH::300 mb",
+        "q350": "pgrb2b::SPFH::350 mb",
+        "q400": "pgrb2b::SPFH::400 mb",
+        "q450": "pgrb2b::SPFH::450 mb",
+        "q500": "pgrb2b::SPFH::500 mb",
+        "q550": "pgrb2b::SPFH::550 mb",
+        "q600": "pgrb2b::SPFH::600 mb",
+        "q650": "pgrb2b::SPFH::650 mb",
+        "q700": "pgrb2b::SPFH::700 mb",
+        "q750": "pgrb2b::SPFH::750 mb",
+        "q800": "pgrb2b::SPFH::800 mb",
+        "q850": "pgrb2b::SPFH::850 mb",
+        "q900": "pgrb2b::SPFH::900 mb",
+        "q925": "pgrb2b::SPFH::925 mb",
+        "q950": "pgrb2b::SPFH::950 mb",
+        "q975": "pgrb2b::SPFH::975 mb",
+        "q1000": "pgrb2b::SPFH::1000 mb",
+    }
+
+    @classmethod
+    def get_item(cls, val: str) -> tuple[str, Callable]:
+        """Get item from GFS vocabulary."""
+        gfs_key = cls.VOCAB[val]
+        if gfs_key.split("::")[0] == "HGT":
+
+            def mod(x: np.array) -> np.array:
+                """Modify data value (if necessary)."""
+                return x * 9.81
+
+        else:
+
+            def mod(x: np.array) -> np.array:
+                """Modify data value (if necessary)."""
+                return x
+
+        return gfs_key, mod
---
+++ test/data/test_gefs.py
@@ -0,0 +1,202 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pathlib
+import shutil
+from datetime import datetime, timedelta
+
+import numpy as np
+import pytest
+
+from earth2studio.data import GEFS_FX
+
+
+@pytest.mark.slow
+@pytest.mark.xfail
+@pytest.mark.timeout(30)
+@pytest.mark.parametrize(
+    "time,lead_time,variable",
+    [
+        (
+            datetime(year=2020, month=11, day=1),
+            [timedelta(hours=3), timedelta(hours=384)],
+            "t2m",
+        ),
+        (
+            [
+                datetime(year=2021, month=8, day=8, hour=6),
+                datetime(year=2022, month=4, day=20, hour=12),
+            ],
+            timedelta(hours=0),
+            ["msl"],
+        ),
+        (
+            datetime(year=2020, month=11, day=1),
+            [timedelta(hours=240)],
+            np.array(["tcwv", "u10m"]),
+        ),
+    ],
+)
+def test_gefs_fetch(time, lead_time, variable):
+
+    ds = GEFS_FX(cache=False)
+    data = ds(time, lead_time, variable)
+    shape = data.shape
+
+    if isinstance(variable, str):
+        variable = [variable]
+
+    if isinstance(lead_time, timedelta):
+        lead_time = [lead_time]
+
+    if isinstance(time, datetime):
+        time = [time]
+
+    assert shape[0] == len(time)
+    assert shape[1] == len(lead_time)
+    assert shape[2] == len(variable)
+    assert shape[3] == 361
+    assert shape[4] == 720
+    assert not np.isnan(data.values).any()
+    assert GEFS_FX.available(time[0])
+    assert np.array_equal(data.coords["variable"].values, np.array(variable))
+
+
+@pytest.mark.slow
+@pytest.mark.xfail
+@pytest.mark.timeout(30)
+@pytest.mark.parametrize("product", ["gec00", "gep01", "gep30"])
+def test_gefs_products(product):
+    time = datetime(year=2022, month=12, day=25)
+    lead_time = timedelta(hours=3)
+    variable = "u100m"
+
+    ds = GEFS_FX(product, cache=False)
+    data = ds(time, lead_time, variable)
+    shape = data.shape
+
+    assert shape[0] == 1
+    assert shape[1] == 1
+    assert shape[2] == 1
+    assert shape[3] == 361
+    assert shape[4] == 720
+    assert not np.isnan(data.values).any()
+    assert np.array_equal(data.coords["variable"].values, np.array([variable]))
+
+
+@pytest.mark.slow
+@pytest.mark.xfail
+@pytest.mark.timeout(30)
+@pytest.mark.parametrize(
+    "time",
+    [
+        np.array([np.datetime64("2024-01-01T00:00")]),
+    ],
+)
+@pytest.mark.parametrize("variable", [["t2m", "msl"]])
+@pytest.mark.parametrize("cache", [True, False])
+def test_gefs_cache(time, variable, cache):
+
+    lead_time = np.array([np.timedelta64(3, "h")])
+
+    ds = GEFS_FX(cache=cache)
+    data = ds(time, lead_time, variable)
+    shape = data.shape
+
+    assert shape[0] == 1
+    assert shape[1] == 1
+    assert shape[2] == 2
+    assert shape[3] == 361
+    assert shape[4] == 720
+    assert not np.isnan(data.values).any()
+    assert GEFS_FX.available(time[0])
+    # Cahce should be present
+    assert pathlib.Path(ds.cache).is_dir() == cache
+
+    # Load from cach or refetch
+    data = ds(time, lead_time, variable[0])
+    shape = data.shape
+
+    assert shape[0] == 1
+    assert shape[1] == 1
+    assert shape[2] == 1
+    assert shape[3] == 361
+    assert shape[4] == 720
+    assert not np.isnan(data.values).any()
+    assert GEFS_FX.available(time[0])
+
+    try:
+        shutil.rmtree(ds.cache)
+    except FileNotFoundError:
+        pass
+
+
+@pytest.mark.timeout(15)
+@pytest.mark.parametrize(
+    "time",
+    [
+        datetime(year=2020, month=9, day=22),
+        datetime.now(),
+    ],
+)
+def test_gefs_available(time):
+    variable = ["mpl"]
+    lead_time = timedelta(hours=0)
+    assert not GEFS_FX.available(time)
+    with pytest.raises(ValueError):
+        ds = GEFS_FX(cache=False)
+        ds(time, lead_time, variable)
+
+
+@pytest.mark.timeout(5)
+@pytest.mark.parametrize(
+    "lead_time",
+    [
+        timedelta(hours=-1),
+        [timedelta(hours=2), timedelta(hours=2, minutes=1)],
+        np.array([np.timedelta64(243, "h")]),
+        np.array([np.timedelta64(390, "h")]),
+    ],
+)
+def test_gefs_invalid_lead(lead_time):
+    time = datetime(year=2022, month=12, day=25)
+    variable = "t2m"
+    with pytest.raises(ValueError):
+        ds = GEFS_FX(cache=False)
+        ds(time, lead_time, variable)
+
+
+@pytest.mark.timeout(5)
+@pytest.mark.parametrize(
+    "variable",
+    ["aaa", "t1m"],
+)
+def test_gefs_invalid_variable(variable):
+    time = datetime(year=2022, month=12, day=25)
+    lead_time = timedelta(hours=0)
+    with pytest.raises(KeyError):
+        ds = GEFS_FX(cache=False)
+        ds(time, lead_time, variable)
+
+
+@pytest.mark.timeout(5)
+@pytest.mark.parametrize(
+    "product",
+    ["gec0", "gep31", "gep00"],
+)
+def test_gefs_invalid_product(product):
+    with pytest.raises(ValueError):
+        GEFS_FX(product, cache=False)
