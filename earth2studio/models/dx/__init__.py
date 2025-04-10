7351e4c7b062b91faa57aec3771b4a9158c1fe08
gertln <gertl@nvidia.com>
2025-04-10 08:14:04 +0200

    Add derived variable diagnostics (#244)
    * Implemented DerivedVariables class for calculating derived meteorological variables
    * Added methods to compute Vapor Pressure Deficit (VPD), Wind Speed (WS), and Relative Humidity from various input variables
    * Included support for different vertical levels and batch processing
    
    ---------
    
    Co-authored-by: Nicholas Geneva <5533524+NickGeneva@users.noreply.github.com>

--- earth2studio/models/dx/__init__.py
+++ earth2studio/models/dx/__init__.py
@@ -14,8 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from earth2studio.models.dx.identity import Identity  # noqa
-from earth2studio.models.dx.precipitation_afno import PrecipitationAFNO  # noqa
+from earth2studio.models.dx.base import DiagnosticModel
 from earth2studio.models.dx.climatenet import ClimateNet  # noqa
 from earth2studio.models.dx.corrdiff import CorrDiffTaiwan  # noqa
-from earth2studio.models.dx.base import DiagnosticModel
+from earth2studio.models.dx.derived import DerivedRH, DerivedVPD, DerivedWS
+from earth2studio.models.dx.identity import Identity  # noqa
+from earth2studio.models.dx.precipitation_afno import PrecipitationAFNO  # noqa
