68ff819abba71b51cc8da797fd5222ae4df7ddf0
Nicholas Geneva <5533524+NickGeneva@users.noreply.github.com>
2025-06-04 16:20:28 -0700

    Climate in a Bottle (cBottle) data source (#343)
    * Adding cbottle to toml
    * Added data source
    * Multiple times working
    * Updates with variables
    * Docs
    * Adding batches
    * Adding kwargs to load package
    * Adding tests
    * Update with new NGC package

--- earth2studio/data/__init__.py
+++ earth2studio/data/__init__.py
@@ -18,6 +18,7 @@ import warnings
 
 from .arco import ARCO
 from .base import DataSource, ForecastSource
+from .cbottle import CBottle3D
 from .cds import CDS
 from .const import Constant, Constant_FX
 from .gefs import GEFS_FX, GEFS_FX_721x1440
