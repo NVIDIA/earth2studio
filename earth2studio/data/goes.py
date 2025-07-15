e2fe8ddd3a01ce4cc09369c4705f5dc97316498e
Oliver <loliverhennigh101@gmail.com>
2025-07-15 12:02:06 -0700

    coveriage

--- earth2studio/data/goes.py
+++ earth2studio/data/goes.py
@@ -320,12 +320,11 @@ class GOES:
         if not self._cache:
             shutil.rmtree(self.cache)
 
-        # Close aiohttp client if s3fs
+        # Close aiohttp client
         # https://github.com/fsspec/s3fs/issues/943
         # https://github.com/zarr-developers/zarr-python/issues/2901
-        if isinstance(self.fs, s3fs.S3FileSystem):
-            await self.fs.set_session()  # Make sure the session was actually initalized
-            s3fs.S3FileSystem.close_session(asyncio.get_event_loop(), self.fs.s3)
+        await self.fs.set_session()  # Make sure the session was actually initalized
+        s3fs.S3FileSystem.close_session(asyncio.get_event_loop(), self.fs.s3)
 
         return xr_array
 
