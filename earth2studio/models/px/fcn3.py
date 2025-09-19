e93932ee62e1dbdcc1a8a5cc23b7e9d74f3749ce
Dallas Foster <dallasf@nvidia.com>
2025-09-19 00:22:13 -0700

    add proper noise handling for fcn3

--- earth2studio/models/px/fcn3.py
+++ earth2studio/models/px/fcn3.py
@@ -179,6 +179,8 @@ class FCN3(torch.nn.Module, AutoModelMixin, PrognosticMixin):
 
         self.set_rng(reset=True, seed=seed)
 
+        self._internal_noise_states = []
+
     def __str__(self) -> str:
         return "fcn3"
 
