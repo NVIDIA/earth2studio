c72ade6dc01c3828f0d8756a7f0287f13b266e68
Oliver <loliverhennigh101@gmail.com>
2025-09-23 10:25:56 -0700

    fixt test

--- test/models/px/test_aifs.py
+++ test/models/px/test_aifs.py
@@ -309,8 +309,8 @@ def test_aifs_package(device, model):
     # Test the cached model package AIFS
     p = model.to(device)
 
-    assert len(p.get_input_variables()) == 94
-    assert len(p.get_output_variables()) == 106
+    assert len(p.input_variables) == 94
+    assert len(p.output_variables) == 106
 
     # Create "domain coords"
     dc = {k: p.input_coords()[k] for k in ["lat", "lon"]}
