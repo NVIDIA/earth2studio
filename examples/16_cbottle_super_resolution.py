7783178fc736a69561db8a00140db5afd8eeb2a6
Nicholas Geneva <ngeneva@nvidia.com>
2025-11-12 19:32:08 +0000

    Bump version to 0.10.0-rc

--- examples/16_cbottle_super_resolution.py
+++ examples/16_cbottle_super_resolution.py
@@ -37,7 +37,7 @@ In this example you will learn:
 """
 # /// script
 # dependencies = [
-#   "earth2studio[cbottle] @ git+https://github.com/NVIDIA/earth2studio.git",
+#   "earth2studio[cbottle] @ git+https://github.com/NVIDIA/earth2studio.git@0.10.0",
 #   "cartopy",
 # ]
 # ///
