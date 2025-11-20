975c96fa34643f8f59f3c4c1dff451f3dde417da
gertl <gertl@nvidia.com>
2025-11-20 16:07:26 +0100

    refactor: extract hard-coded variable list to class constant

--- earth2studio/models/dx/corrdiff.py
+++ earth2studio/models/dx/corrdiff.py
@@ -1243,6 +1243,56 @@ class CorrDiffCMIP6(CorrDiff):
     - Includes time dimension in coordinate system
     """
 
+    # Variables that must be non-negative (clipped to min=0 during postprocessing)
+    # These represent physical quantities that cannot be negative (temperature, pressure, etc.)
+    _NONNEGATIVE_VARS = [
+        "t2m",
+        "sp",
+        "msl",
+        "tcwv",
+        "z50",
+        "z100",
+        "z150",
+        "z200",
+        "z250",
+        "z300",
+        "z400",
+        "z500",
+        "z600",
+        "z700",
+        "z850",
+        "z925",
+        "z1000",
+        "t50",
+        "t100",
+        "t150",
+        "t200",
+        "t250",
+        "t300",
+        "t400",
+        "t500",
+        "t600",
+        "t700",
+        "t850",
+        "t925",
+        "t1000",
+        "q50",
+        "q100",
+        "q150",
+        "q200",
+        "q250",
+        "q300",
+        "q400",
+        "q500",
+        "q600",
+        "q700",
+        "q850",
+        "q925",
+        "q1000",
+        "sst",
+        "d2m",
+    ]
+
     def __init__(
         self,
         *args: Any,
@@ -1477,55 +1527,13 @@ class CorrDiffCMIP6(CorrDiff):
         # denormalize
         x = x * self.out_scale + self.out_center
 
-        var_limited_0 = [
-            "t2m",
-            "sp",
-            "msl",
-            "tcwv",
-            "z50",
-            "z100",
-            "z150",
-            "z200",
-            "z250",
-            "z300",
-            "z400",
-            "z500",
-            "z600",
-            "z700",
-            "z850",
-            "z925",
-            "z1000",
-            "t50",
-            "t100",
-            "t150",
-            "t200",
-            "t250",
-            "t300",
-            "t400",
-            "t500",
-            "t600",
-            "t700",
-            "t850",
-            "t925",
-            "t1000",
-            "q50",
-            "q100",
-            "q150",
-            "q200",
-            "q250",
-            "q300",
-            "q400",
-            "q500",
-            "q600",
-            "q700",
-            "q850",
-            "q925",
-            "q1000",
-            "sst",
-            "d2m",
+        # Clip non-negative variables (temperature, pressure, humidity, etc.)
+        # Identify indices of non-negative variables in self.output_variables
+        indices = [
+            i
+            for i, v in enumerate(self.output_variables)
+            if v in self._NONNEGATIVE_VARS
         ]
-        # identify indices of var_limited_0 in self.output_variables
-        indices = [i for i, v in enumerate(self.output_variables) if v in var_limited_0]
 
         # clip x to enforce lower bound of 0 along the channel dimension
         x[:, indices] = x[:, indices].clamp(min=0)
