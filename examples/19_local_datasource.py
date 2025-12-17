4d1ff19fd9f5f00bba89b80f97552b0394cd7f75
Nicholas Geneva <ngeneva@nvidia.com>
2025-12-17 03:41:19 +0000

    Bump version to 0.11.0-rc

--- examples/19_local_datasource.py
+++ examples/19_local_datasource.py
@@ -29,7 +29,7 @@ This example demonstrates how to:
 """
 # /// script
 # dependencies = [
-#   "earth2studio @ git+https://github.com/NVIDIA/earth2studio.git",
+#   "earth2studio @ git+https://github.com/NVIDIA/earth2studio.git@0.11.0",
 #   "aurora",
 #   "cartopy",
 # ]
