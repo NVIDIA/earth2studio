2bb7af42649f04f488a567a92f646d08851affd4
Nicholas Geneva <ngeneva@nvidia.com>
2025-12-17 15:06:37 +0000

    Fix optional dep

--- examples/19_local_datasource.py
+++ examples/19_local_datasource.py
@@ -29,8 +29,7 @@ This example demonstrates how to:
 """
 # /// script
 # dependencies = [
-#   "earth2studio @ git+https://github.com/NVIDIA/earth2studio.git",
-#   "aurora",
+#   "earth2studio[aurora] @ git+https://github.com/NVIDIA/earth2studio.git",
 #   "cartopy",
 # ]
 # ///
