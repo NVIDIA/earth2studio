46c741dd5d0508ecae5eba676ad4fdca2f125216
Nicholas Geneva <ngeneva@nvidia.com>
2026-01-12 18:18:13 +0000

    Update fill method

--- earth2studio/models/px/aifs.py
+++ earth2studio/models/px/aifs.py
@@ -363,7 +363,7 @@ class AIFS(torch.nn.Module, AutoModelMixin, PrognosticMixin):
         """Load prognostic from package"""
 
         # Load model
-        model_path = package.resolve("aifs-single-mse-1.0.ckpt")
+        model_path = package.resolve("aifs-single-mse-1.1.ckpt")
         model = torch.load(
             model_path, weights_only=False, map_location=torch.ones(1).device
         )
@@ -846,13 +846,15 @@ class AIFS(torch.nn.Module, AutoModelMixin, PrognosticMixin):
         out[:, :, 0, indices[valid_mask]] = x[0, 0, 0, ...]
         out[:, :, 1, indices[valid_mask]] = x[0, 0, 1, ...]
 
-        # Drop generated forcing dimension range from output
-        out = torch.cat([out[:, :, :, :92, ...], out[:, :, :, 101:, ...]], dim=3)
+        # Drop generated forcing / invariants from output
+        all_indices = torch.arange(len(VARIABLES))
+        keep = torch.isin(
+            all_indices, self.model.data_indices.data.output.forcing, invert=True
+        )
+        out = out[:, :, :, keep, ...]
 
         # Update coordinates with remaining variable names
-        all_indices = torch.arange(len(VARIABLES))
-        variable_mask = ~torch.isin(all_indices, self.forcing_ids)
-        selected_variables = [VARIABLES[i] for i in all_indices[variable_mask].tolist()]
+        selected_variables = [VARIABLES[i] for i in all_indices[keep].tolist()]
 
         out_coords = coords.copy()
         out_coords["variable"] = np.array(selected_variables)
