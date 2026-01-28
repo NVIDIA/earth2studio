897f0814ad93f9e64096c9164839f3377db6c0b2
Nicholas Geneva <ngeneva@nvidia.com>
2026-01-28 00:02:49 +0000

    Update version to 0.13.0a0

--- examples/13_cyclone_tracking.py
+++ examples/13_cyclone_tracking.py
@@ -36,7 +36,7 @@ In this example you will learn:
 """
 # /// script
 # dependencies = [
-#   "earth2studio[cyclone,sfno] @ git+https://github.com/NVIDIA/earth2studio.git@0.12.0",
+#   "earth2studio[cyclone,sfno] @ git+https://github.com/NVIDIA/earth2studio.git",
 #   "cartopy",
 # ]
 # ///
