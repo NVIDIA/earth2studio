7f2fe873acf2cda1f2c374f46cefdd3078eb20ef
Nicholas Geneva <ngeneva@nvidia.com>
2026-01-29 19:53:00 +0000

    Update version to 0.13.0a0 (patch)

--- examples/08_distributed_manager.py
+++ examples/08_distributed_manager.py
@@ -38,7 +38,7 @@ In this example you will learn:
 """
 # /// script
 # dependencies = [
-#   "earth2studio[dlwp] @ git+https://github.com/NVIDIA/earth2studio.git@0.12.1",
+#   "earth2studio[dlwp] @ git+https://github.com/NVIDIA/earth2studio.git",
 #   "matplotlib",
 # ]
 # ///
