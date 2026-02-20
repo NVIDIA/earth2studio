43dcd2b2ebcd33ad9bd6413b86e11866deee5a4e
Isaac <irlyngaas@github.com>
2026-02-20 10:29:03 -0500

    Updating launch_test

--- launch_test.py
+++ launch_test.py
@@ -1,8 +1,8 @@
 import numpy as np
-from earth2studio.run_orbit import run
+#from earth2studio.run_orbit import run
+from earth2studio.run_orbit_clean import run
 
 from earth2studio.data import NCAR_ERA5
-#from earth2studio.data import WB2ERA5
 
 from earth2studio.io import ZarrBackend
 from earth2studio.lexicon.ncar import NCAR_ERA5Lexicon
@@ -12,10 +12,14 @@ package = OrbitGlobalPrecip9_5M.load_default_package()
 orbit = OrbitGlobalPrecip9_5M.load_model(package, "global", "9.5m", "precipitation")
 
 data = NCAR_ERA5()
-#data = WB2ERA5()
-io = ZarrBackend("outputs/aifs_forecast.zarr")
+file_name = "outputs/aifs_forecast.zarr"
+io = ZarrBackend(file_name)
 
-#NCAR_ERA5
-time = np.datetime64('2025-01-01T12:00:00')
-#run(["2025-01-02T00:00:00"], orbit, data, io)
-run([time], orbit, data, io)
+time = np.datetime64('2020-01-01T00:00:00')
+
+data_check = True
+inference_check = True
+inference_check_file = '/lustre/orion/stf006/world-shared/irl1/01012020_00_00_00.npy'
+plot_inference = True
+
+run([time], orbit, data, io, file_name, data_check, inference_check, inference_check_file, plot_inference)
