794955162e019a10cfe8a90789554a0812d2c86b
Nicholas Geneva <5533524+NickGeneva@users.noreply.github.com>
2026-03-03 11:23:17 -0800

    Update Cbottle SR with CBottle patch (#727)
    * Update cbottle version
    * Change log

--- examples/16_cbottle_super_resolution.py
+++ examples/16_cbottle_super_resolution.py
@@ -92,6 +92,7 @@ cbottle_sr = CBottleSR.load_model(
     package,
     output_resolution=(1024, 1024),
     super_resolution_window=super_resolution_window,
+    distilled_model=True,
 )
 
 # Load the infill model
