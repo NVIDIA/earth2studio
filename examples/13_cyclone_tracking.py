126a7496ee1d96ac8e05144566f8a50277f78f32
Nicholas Geneva <ngeneva@nvidia.com>
2026-03-18 07:29:47 +0000

    Bump version to 0.13.0-rc

--- examples/13_cyclone_tracking.py
+++ examples/13_cyclone_tracking.py
@@ -37,7 +37,7 @@ In this example you will learn:
 # /// script
 # dependencies = [
 #   "torch==2.9.1", # Match lock file to avoid torch-harmonics issue
-#   "earth2studio[cyclone,sfno] @ git+https://github.com/NVIDIA/earth2studio.git",
+#   "earth2studio[cyclone,sfno] @ git+https://github.com/NVIDIA/earth2studio.git@0.13.0",
 #   "cartopy",
 # ]
 # ///
