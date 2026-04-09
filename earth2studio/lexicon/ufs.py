31ff2b9c142b747b5441191b8826cfe4cdc2f28a
Nicholas Geneva <ngeneva@nvidia.com>
2026-04-09 01:14:08 +0000

    Fix

--- earth2studio/lexicon/ufs.py
+++ earth2studio/lexicon/ufs.py
@@ -121,7 +121,7 @@ class GSISatelliteLexicon(metaclass=LexiconType):
         "amsua": "metop-a,metop-b,metop-c,n15,n16,n17,n18,n19::amsua::ges::Observation",
         "amsub": "n15,n16,n17::amsub::ges::Observation",
         "iasi": "metop-a,metop-b,metop-c::iasi::ges::Observation",
-        "cris": "npp,n20::cris-fsr::ges::Observation",  # Cross-track Infrared Sounder, Full Spectral Resolution (CrIS-FSR)
+        "crisfr": "npp,n20::cris-fsr::ges::Observation",
     }
 
     @classmethod
