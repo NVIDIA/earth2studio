4dc95e9dc268ba742c2a72ad226457be2801251c
Isaac <irlyngaas@github.com>
2026-05-05 14:12:53 -0400

    Removing testing for PR to main NVIDIA repo

--- launch_test.py
+++ launch_test.py
@@ -1,10 +1,7 @@
 import numpy as np
 from earth2studio.run_orbit import run
-
 from earth2studio.data import NCAR_ERA5
-
 from earth2studio.io import ZarrBackend
-from earth2studio.lexicon.ncar import NCAR_ERA5Lexicon
 from earth2studio.models.dx import OrbitGlobalPrecip
 
 #time = [np.datetime64('2020-01-01T00:00:00')]
@@ -15,14 +12,7 @@ orbit = OrbitGlobalPrecip.load_model(package, "global", "9.5m", "precipitation")
 #orbit = OrbitGlobalPrecip.load_model(package, "global", "126m", "precipitation")
 
 data = NCAR_ERA5()
-file_name = "outputs/aifs_forecast.zarr"
+file_name = "outputs/orbit.zarr"
 io = ZarrBackend(file_name)
 
-
-data_check = True
-inference_check = True
-inference_check_file = '/lustre/orion/stf006/proj-shared/irl1/earth2studio/ORBIT-2-e2s/examples/0_preds.npy'
-check_metrics = False
-plot_inference = True
-
-run(time, orbit, data, io, file_name, data_check, inference_check, inference_check_file, check_metrics, plot_inference)
+run(time, orbit, data, io)
