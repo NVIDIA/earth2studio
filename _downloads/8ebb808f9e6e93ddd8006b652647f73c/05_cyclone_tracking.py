c6bd957906a6bb672c11e6f59ba819ff2fb2d0db
Nicholas Geneva <ngeneva@nvidia.com>
2026-05-18 03:07:43 +0000

    [CI] Full Docs Build 34ebf90

--- _downloads/8ebb808f9e6e93ddd8006b652647f73c/05_cyclone_tracking.py
+++ _downloads/8ebb808f9e6e93ddd8006b652647f73c/05_cyclone_tracking.py
@@ -36,7 +36,7 @@ In this example you will learn:
 """
 # /// script
 # dependencies = [
-#   "torch==2.9.1", # Match lock file to avoid torch-harmonics issue
+#   "torch==2.11.0", # Match lock file to avoid torch-harmonics issue
 #   "earth2studio[cyclone,sfno] @ git+https://github.com/NVIDIA/earth2studio.git",
 #   "cartopy",
 # ]
