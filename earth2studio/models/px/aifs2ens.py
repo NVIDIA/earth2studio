09814bbcc9359ca035e7e1c367a87c37228066e5
Nicholas Geneva <ngeneva@nvidia.com>
2026-05-21 02:12:11 +0000

    adding install steps

--- earth2studio/models/px/aifs2ens.py
+++ earth2studio/models/px/aifs2ens.py
@@ -539,7 +539,7 @@ class AIFS2ENS(torch.nn.Module, AutoModelMixin, PrognosticMixin):
             ifs = IFS(cache=True, verbose=False)
             invariants, _ = fetch_data(
                 source=ifs,
-                time=np.array([np.datetime64("2026-05-01T00:00:00")]),
+                time=np.array([np.datetime64("2026-05-15T00:00:00")]),
                 variable=["lsm", "sdor", "slor", "z", "wmb"],
             )
             invariants = invariants.squeeze()
